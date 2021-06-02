d8f869aa1acd754cfe21b2b858a90980d18af7c3
pingwindyktator <ja2222ja@gmail.com>
2021-06-02 18:05:32 +0200

    Add crowdsale info

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import utils
 app = Flask(__name__)
 w3 = Web3(Web3.HTTPProvider(f'https://mainnet.infura.io/v3/{config.INFURA_PROJECT_ID}'))
 token_contract = w3.eth.contract(config.TOKEN_ADDRESS, abi=config.TOKEN_ABI)
+crowdsale_contract = w3.eth.contract(config.CROWDSALE_ADDRESS, abi=config.CROWDSALE_ABI)
 
 
 @utils.TimedLruCache(expiration=timedelta(minutes=config.UPDATE_INTERVAL_MINUTES))
@@ -15,9 +16,43 @@ def fetchTotalSupply():
     return str(token_contract.functions.totalSupply().call())
 
 
+@utils.TimedLruCache(expiration=timedelta(minutes=config.UPDATE_INTERVAL_MINUTES))
+def fetchCrowdsaleRaisedWei():
+    return str(crowdsale_contract.functions.weiRaised().call())
+
+
+@utils.TimedLruCache(expiration=timedelta(minutes=config.UPDATE_INTERVAL_MINUTES))
+def fetchCrowdsaleRaisedTokens():
+    return str(int(fetchCrowdsaleRaisedWei) * int(crowdsale_contract.functions.rate().call()))
+
+
+@utils.TimedLruCache
+def fetchCrowdsaleCap():
+    return str(crowdsale_contract.functions.cap().call())
+
+
+# endpoints
+
+
 @app.route('/totalSupply')
 def getTotalSupply():
     return fetchTotalSupply()
+
+
+@app.route('/crowdsaleRaisedWei')
+def getCrowdsaleRaisedWei():
+    return fetchCrowdsaleRaisedWei()
+
+
+@app.route('/crowdsaleRaisedTokens')
+def getCrowdsaleRaisedTokens():
+    return fetchCrowdsaleRaisedTokens()
+
+
+@app.route('/crowdsaleCap')
+def getCrowdsaleCap():
+    # cap is the maximum amount of wei accepted in the crowdsale
+    return fetchCrowdsaleCap()
 
 
 if __name__ == '__main__':
